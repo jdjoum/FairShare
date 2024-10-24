895fa33fd084a82103718c9f95430ed06bc2901a
jdjoum <jdjoum@outlook.com>
2024-10-23 20:32:10 -0400

    Initial Commit

---
+++ FairShare/FairShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FairShareApp.swift
+//  FairShare
+//
+//  Created by Julian-Justin Djoum on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FairShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
